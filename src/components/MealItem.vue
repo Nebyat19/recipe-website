e184e9253a707d2643280237cb84cfed79d5baef
Nebyat Bekele <nebyat.bekele@aastustudent.edu.et>
2024-05-18 14:59:47 +0300

    code refactoring and responsiveness

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-white rounded-t-xl overflow-hidden border shadow">
     <router-link :to="{ name: 'mealDetail', params: { id: meal.idMeal } }">
-   <div class=" relative">
-    <img :src="meal.strMealThumb" alt="strMeal" class=" h-48 w-full object-cover" />
+   <div  class="bg-gray-100 h-48 relative">
+  <img class="absolute h-48 top-0 z-0" :src="BLACK_LOGO"/>
     <span v-if="meal.strCategory" class="absolute top-2 left-2 px-3 py-2 text-xs rounded-lg bg-green-100 text-green-600">{{meal.strCategory}}</span>
+
+  <img :src="meal.strMealThumb" alt="strMeal" class="absolute  z-50 h-48 w-full object-cover" />
    </div>
    
     </router-link>
@@ -22,6 +24,7 @@
 
 <script setup>
 import YouTubeButton from '@/components/YouTubeButton.vue'
+import { BLACK_LOGO } from '@/lib/constants.js'
 const props = defineProps({
   meal: Object
 })
